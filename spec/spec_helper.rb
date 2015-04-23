3a2c2a83bd16b00f6099adc1887bb531b1805e3e
Sander Kuijper <sajoku@me.com>
2015-04-23 14:25:52 +0200

    Add pry as dev dependency
    * Add spec default configure options with some  sane defaults.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bucky'
+require "pry"
+
+Dir[File.dirname(__FILE__) + "/support/**.rb"].each{|support_file| require support_file }
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.mock_with :rspec
+end
